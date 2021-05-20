c68eae9c5221533ef4564d0725d69d4ef638bff9
Dantence <84451807+Dantence@users.noreply.github.com>
2021-05-20 16:41:04 +0800

    Create head.h

---
+++ head.h
@@ -0,0 +1,17 @@
+#pragma once
+#include<iostream>
+#include<cmath>
+#include<stdexcept>
+#include<string>
+#include<array>
+#include<numeric>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<utility>
+#include<unordered_map>
+#include<memory>
+#include<sstream>
+#include<istream>
+using namespace std;
